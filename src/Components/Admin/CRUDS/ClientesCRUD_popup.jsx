05781c770c7a763f66a4cb587ea846c55502479e
Facundo Romano <facundo-romano@hotmail.com>
2023-11-18 19:57:38 -0300

    cliente y marca popup implemented

--- src/Components/Admin/CRUDS/ClientesCRUD_popup.jsx
+++ src/Components/Admin/CRUDS/ClientesCRUD_popup.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Button, Col, FormControl } from 'react-bootstrap';
+import { Button, Col, FormControl, FormGroup, Modal } from 'react-bootstrap';
 import { createCliente, updateCliente } from '../../../Functions/ClienteFunctions';
 
 const ClientesCRUD_popup = ({ cliente, onClienteUpdated, closePopUp }) => {
@@ -54,55 +54,61 @@ const ClientesCRUD_popup = ({ cliente, onClienteUpdated, closePopUp }) => {
     //#endregion
   
     return (
-    <Col xs={5} className="bg-light p-2 mt-4 ">
+    <Modal centered show={true}>
+    <Modal.Header>
+      { cliente ? <h4>Editar cliente</h4> : <h4>Añadir cliente</h4> }
+    </Modal.Header>
 
-    { cliente ? <h4>Editar cliente</h4> : <h4>Añadir cliente</h4> }
+    <Modal.Body>
+      
+      <FormGroup>
+        <FormControl
+            type="text"
+            className="mb-2"
+            placeholder="Nombre"
+            name="nombre"
+            value={clienteData.nombre}
+            onChange={handleInputChange}
+        />
+        
+        <FormControl
+            type="text"
+            className="mb-2"
+            placeholder="Apellido"
+            name="apellido"
+            value={clienteData.apellido}
+            onChange={handleInputChange}
+        />
+        
+        <FormControl
+            type="text"
+            className="mb-2"
+            placeholder="Username"
+            name="username"
+            value={clienteData.username}
+            onChange={handleInputChange}
+        />
+        
+        <FormControl
+            type="password"
+            className="mb-2"
+            placeholder="Password"
+            name="password"
+            value={clienteData.password}
+            onChange={handleInputChange}
+        />
+      </FormGroup>
 
-    <FormControl
-        type="text"
-        className="mb-2"
-        placeholder="Nombre"
-        name="nombre"
-        value={clienteData.nombre}
-        onChange={handleInputChange}
-    />
-    
-    <FormControl
-        type="text"
-        className="mb-2"
-        placeholder="Apellido"
-        name="apellido"
-        value={clienteData.apellido}
-        onChange={handleInputChange}
-    />
-    
-    <FormControl
-        type="text"
-        className="mb-2"
-        placeholder="Username"
-        name="username"
-        value={clienteData.username}
-        onChange={handleInputChange}
-    />
-    
-    <FormControl
-        type="password"
-        className="mb-2"
-        placeholder="Password"
-        name="password"
-        value={clienteData.password}
-        onChange={handleInputChange}
-    />
-    
-    {
-    cliente ? 
-        <Button onClick={handleGuardar}>Guardar cambios</Button>
-        :
-        <Button onClick={handleGuardar}>Crear cliente</Button>
-    }
-        <Button className='m-2' onClick={() => closePopUp()}>Canclear</Button>
+        { cliente ? 
+            <Button onClick={handleGuardar}>Guardar cambios</Button>
+            :
+            <Button onClick={handleGuardar}>Crear cliente</Button>
+        }
 
-    </Col>
+      <Button className='m-2' onClick={() => closePopUp()}>Canclear</Button>
+    </Modal.Body>
+
+    </Modal>
   );
 };
 
--- src/Components/Admin/CRUDS/MarcasCRUD.jsx
+++ src/Components/Admin/CRUDS/MarcasCRUD.jsx
@@ -1,83 +1,58 @@
-import { useState, useEffect } from "react";
-import { Row, Col, Form, Button } from "react-bootstrap";
-
-import {
-  getAllMarcas,
-  createMarca,
-  updateMarca,
-  deleteMarca,
-} from "../../../Functions/MarcasFunctions";
+import { useState, useEffect } from 'react';
+import { getAllMarcas, deleteMarca } from '../../../Functions/MarcasFunctions';
+import MarcasPopUp from './MarcasCRUD_popup';
+import { Col, Button } from 'react-bootstrap';
 
 const MarcasCRUD = () => {
+  //#region Declaracion useState's
   const [marcas, setMarcas] = useState([]);
-  const [creating, setCreating] = useState(false);
-  const [editing, setEditing] = useState(false);
-  const [formData, setFormData] = useState({ nombre: "" });
-  const [editData, setEditData] = useState({});
+  const [popUp, setPopUp] = useState(false);
+  const [selectedMarca, setSelectedMarca] = useState(null);
+  //#endregion
 
+  //#region Data inicial useEffect(clientes)
   const fetchData = async () => {
     try {
-      const response = await getAllMarcas();
-      setMarcas(response.data);
-    } catch (error) {
-      console.error(error);
+      const marcasRespone = await getAllMarcas();
+      setMarcas(marcasRespone.data);
+    } catch (e) {
+      console.error(e.message);
     }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
+  //#endregion
+  
+  const openPopup = (marca) => {
+    setSelectedMarca(marca);
+    setPopUp(true);
+  };
+  //#endregion
 
-  const handleCreate = async () => {
-    try {
-      await createMarca(formData);
-      setCreating(false);
-      setFormData({ nombre: "" });
-      fetchData();
-    } catch (error) {
-      console.error(error);
+  //#region Handle elminar cliente
+  const handleDelete = async (idMarca) => {
+    try{
+      const response = await deleteMarca(idMarca)
+      console.log('Usuario eliminado', response)
+      setPopUp(false)
     }
-  };
-
-  const handleEditOpen = (marca) => {
-    setCreating(false);
-    setEditing(true);
-    setEditData(marca);
-    setFormData({ nombre: marca.nombre });
-  };
-
-  const handleUpdate = async () => {
-    try {
-      await updateMarca(editData.id, formData);
-      setEditing(false);
-      setEditData({});
-      setFormData({ nombre: "" });
-      fetchData();
-    } catch (error) {
-      console.error(error);
+    catch (e)
+    {
+      return e.message
     }
+    fetchData();
   };
-
-  const handleDelete = async (id) => {
-    try {
-      await deleteMarca(id);
-      fetchData();
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  //#endregion
 
   return (
-    <>
-      <Col xs={12}>
+    <div>
+      <Col xs={5}>
         <Button
           variant="outline-light"
           className="mt-3"
-          onClick={() => {
-            setCreating(true);
-            setEditing(false);
-            setFormData({ nombre: "" });
-          }}
+          onClick={() => openPopup(null)}
         >
           Crear Marca
         </Button>
@@ -87,21 +62,17 @@ const MarcasCRUD = () => {
               key={marca.id}
               className="list-group-item d-flex justify-content-between"
             >
-              {marca.nombre}
+              {marca.nombre} 
               <div>
                 <Button
                   variant="warning"
                   size="sm"
                   className="me-1"
-                  onClick={() => handleEditOpen(marca)}
+                  onClick={() => openPopup(marca)}
                 >
                   <span className="material-symbols-outlined">edit</span>
                 </Button>
-                <Button
-                  variant="danger"
-                  size="sm"
-                  onClick={() => handleDelete(marca.id)}
-                >
+                <Button variant="danger" size="sm" onClick={() => handleDelete(marca.id)}>
                   <span className="material-symbols-outlined">delete</span>
                 </Button>
               </div>
@@ -109,65 +80,23 @@ const MarcasCRUD = () => {
           ))}
         </ul>
       </Col>
-      <Row>
-        {creating && (
-          <Col xs={12} className="rounded bg-light mt-3 p-3">
-            <Col className="d-flex justify-content-between">
-              <h6 className="text-black py-3">Crear Marca</h6>
-              <Button
-                size="sm"
-                variant="light"
-                onClick={() => setCreating(false)}
-              >
-                <span className="material-symbols-outlined">close</span>
-              </Button>
-            </Col>
-            <Form.Group>
-              <Form.Control
-                type="text"
-                className="form-control"
-                placeholder="Nombre de la marca"
-                value={formData.nombre || ""}
-                onChange={(e) =>
-                  setFormData({ ...formData, nombre: e.target.value })
-                }
-              />
-            </Form.Group>
-            <Button className="my-3" onClick={handleCreate}>
-              Guardar
-            </Button>
-          </Col>
-        )}
-        {editing && (
-          <Col xs={12} className="rounded bg-light mt-3 p-3">
-            <Col className="d-flex justify-content-between">
-              <h6 className="text-black py-3">Editar Marca</h6>
-              <Button
-                size="sm"
-                variant="light"
-                onClick={() => setEditing(false)}
-              >
-                <span className="material-symbols-outlined">close</span>
-              </Button>
-            </Col>
-            <Form.Group>
-              <Form.Control
-                type="text"
-                className="form-control"
-                placeholder="Nombre de la marca"
-                value={formData.nombre || editData.nombre}
-                onChange={(e) =>
-                  setFormData({ ...formData, nombre: e.target.value })
-                }
-              />
-            </Form.Group>
-            <Button className="my-3" onClick={handleUpdate}>
-              Guardar Cambios
-            </Button>
-          </Col>
-        )}
-      </Row>
-    </>
+        {  
+        //#region Renderizado condicional PopUp
+        popUp ? (
+          <MarcasPopUp 
+          marca={selectedMarca} 
+          onClienteUpdated={() => fetchData()}
+          closePopUp={() => setPopUp(false)}
+          />
+        ) 
+        : 
+        (
+          <>
+          </>
+        )
+        //#endregion
+        }
+    </div>
   );
 };
 
