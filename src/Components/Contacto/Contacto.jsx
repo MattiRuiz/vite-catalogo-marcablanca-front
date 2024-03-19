a808a9efe2d7f721a7fc7efaa3074a390443a1fa
Mati Ruiz <matias.ruiz.dg@gmail.com>
2024-03-18 23:11:56 -0300

    cambios con el kq

---
+++ src/Components/Contacto/Contacto.jsx
@@ -0,0 +1,18 @@
+import { Col, Row } from 'react-bootstrap'
+
+const Contacto = () => {
+  return (
+    <>
+      <Row className="justify-content-center">
+        <Col xs={12} lg={8} className="py-5">
+          <h3>Contacto</h3>
+          <p>
+            Tenes consultas, dudas o recomendaciones? No dudes en escribirnos.{' '}
+          </p>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default Contacto
