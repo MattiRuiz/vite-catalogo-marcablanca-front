71f9af625d681b00e3d2e6b2afc1650909185c2f
mati-ruiz <tutes.4ever@gmail.com>
2024-11-29 12:55:51 -0300

    Cambios realizados para solucionar la indexación por google search

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,28 @@
+import { SitemapStream, streamToPromise } from 'sitemap'
+import { createWriteStream } from 'fs'
+
+const generateSitemap = async () => {
+  const smStream = new SitemapStream({
+    hostname: 'https://www.blanqueriamarcablanca.com',
+  })
+
+  // Añade tus rutas manualmente o de una base de datos
+  const routes = [
+    { url: '/', changefreq: 'weekly', priority: 1.0 },
+    { url: '/catalogo', changefreq: 'daily', priority: 0.8 },
+    { url: '/contacto', changefreq: 'monthly', priority: 0.5 },
+    { url: '/suscripciones', changefreq: 'monthly', priority: 0.2 },
+    { url: '/terminos-y-condiciones', changefreq: 'monthly', priority: 0.1 },
+    // Agrega más rutas aquí
+  ]
+
+  routes.forEach((route) => smStream.write(route))
+
+  smStream.end()
+
+  // Generar y guardar el archivo
+  const sitemap = await streamToPromise(smStream)
+  createWriteStream('./public/sitemap.xml').write(sitemap)
+}
+
+generateSitemap().catch(console.error)
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Cambia esto al directorio que desees, por ejemplo 'build'
+    outDir: 'build', // Cambia esto al directorio que desees, por ejemplo 'build'
   },
-});
+  server: {
+    historyApiFallback: true,
+  },
+})
