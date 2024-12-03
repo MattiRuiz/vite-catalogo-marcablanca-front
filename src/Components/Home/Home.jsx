f2b3c390f2933ab0480bab0085f4be45582fb574
mati-ruiz <tutes.4ever@gmail.com>
2024-12-03 12:51:13 -0300

    Modificado headers de index.html y home.jsx para solucionar problemas de pagina de google search

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react'
 import HomeUnlog from './HomeUnlog'
 import WelcomeLog from '../Welcome/WelcomeLog'
+import { Helmet } from 'react-helmet'
 
 import LoginContext from '../../Context/LoginContext'
 
@@ -13,7 +14,25 @@ function Home() {
 
   const auth = localStorage.getItem('token')
 
-  return <>{auth ? <WelcomeLog /> : <HomeUnlog />}</>
+  return (
+    <>
+      <Helmet>
+        <title>Bienvenidos a Marca Blanca</title>
+
+        <meta
+          name="description"
+          content="Explora el catálogo de productos de Marca Blanca, calidad y los mejores precios de Rosario."
+        />
+        <meta
+          name="keywords"
+          content="blanquería, marca blanca, Rosario, Santa Fe, catálogo, mayorista, reventa, revendedores, rubro blanco, lista de precio"
+        />
+
+        <link rel="canonical" href="https://www.blanqueriamarcablanca.com/" />
+      </Helmet>
+      {auth ? <WelcomeLog /> : <HomeUnlog />}
+    </>
+  )
 }
 
 export default Home
